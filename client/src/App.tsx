f1f2f9c2d2526e94f1d513e8d4707c7724169192
Ariel <arieltanami122@gmail.com>
2025-09-07 12:56:45 +0300

    Use useEffect & useState hooks to laod and save the state of the demo post data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,24 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Post } from "./components/post/Post";
-import type { PostProps } from "./components/post/PostProps.js";
-import { posts } from "./data/postsData.js";
+import type { PostEntity } from "./components/post/PostEntity.js";
+import { posts as postsData } from "./data/postsData.tsx";
 
 export default () => {
-  return (
-    <>
-      {posts.map((p: PostProps) => (
-        <Post key={p.id} {...p}></Post>
-      ))}
-    </>
-  );
+  const [posts, setPosts] = useState<PostEntity[]>([]);
+
+  useEffect(() => {
+    // Load Posts
+    const fetchPosts = () => {
+      setPosts(postsData);
+      console.log("postsData", postsData);
+    };
+    try {
+      fetchPosts();
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  return <>{posts && posts.length > 0 && posts.map((p: PostEntity) => <Post key={p.id} {...p}></Post>)}</>;
 };
