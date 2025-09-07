998d04e140614d916bce2075f947c6fe228c9234
Ariel <arieltanami122@gmail.com>
2025-09-07 15:39:55 +0300

    Use BrowserRouter in Main.tsx and Add routes in App.tsx and pass them to AplicationLayout as a children

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,22 @@ import "./App.css";
 import { Post } from "./components/post/Post";
 import type { PostEntity } from "./components/post/PostEntity.js";
 import { posts as postsData } from "./data/postsData.tsx";
+import AplicationLayout from "./components/application-layout/AplicationLayout.tsx";
+import { Route, Routes } from "react-router-dom";
+import NotFoundPage from "./components/application-layout/NotFoundPage.tsx";
 
 export default () => {
+  return (
+    <AplicationLayout>
+      <Routes>
+        <Route index element={<Posts />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </AplicationLayout>
+  );
+};
+
+function Posts() {
   const [posts, setPosts] = useState<PostEntity[]>([]);
 
   useEffect(() => {
@@ -21,4 +35,4 @@ export default () => {
   }, []);
 
   return <>{posts && posts.length > 0 && posts.map((p: PostEntity) => <Post key={p.id} {...p}></Post>)}</>;
-};
+}
