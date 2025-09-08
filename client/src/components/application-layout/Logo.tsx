bf8c32b8d04ba74720d07b6041cbe2780c15a9e9
Ariel <arieltanami122@gmail.com>
2025-09-08 10:56:36 +0300

    Add TODO: Change the logo from text to image

--- client/src/components/application-layout/Logo.tsx
+++ client/src/components/application-layout/Logo.tsx
@@ -2,6 +2,7 @@ import "./Logo.css";
 import { Link } from "react-router-dom";
 export default () => (
   <Link id="logo" to="/">
+    {/* TODO: Change the logo from text to image */}
     <h1>LinKodkod</h1>
   </Link>
 );
