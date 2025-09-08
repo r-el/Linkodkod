66b33fef6942a0083c3a77f2efdc6cf84f89ea25
Ariel <arieltanami122@gmail.com>
2025-09-08 17:50:40 +0300

    Add `/post/:id` route in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,14 @@ import AplicationLayout from "./components/application-layout/AplicationLayout.t
 import { Route, Routes } from "react-router-dom";
 import NotFoundPage from "./components/application-layout/NotFoundPage.tsx";
 import { Posts } from "./components/posts/Posts.tsx";
+import { PostDetailPage } from "./pages/PostDetailPage.tsx";
 
 export default () => {
   return (
     <AplicationLayout>
       <Routes>
         <Route index element={<Posts />} />
+        <Route path="/post/:id" element={<PostDetailPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </AplicationLayout>
