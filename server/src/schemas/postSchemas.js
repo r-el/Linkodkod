429ac285e1c64275cd1a287fc30e182dc04ac6ad
Ariel <arieltanami122@gmail.com>
2025-09-09 11:50:07 +0300

    Add postSchemas -> - createPostSchema

---
+++ server/src/schemas/postSchemas.js
@@ -0,0 +1,14 @@
+/**
+ * Zod Validation Schemas
+ * Defines validation rules for post endpoints
+ */
+
+import Joi from "joi";
+
+export const createPostSchema = Joi.object({
+  imgSrc: Joi.string().trim().required(),
+  description: Joi.string().trim().required(),
+  likes: Joi.number().integer().required(),
+  author: Joi.string().trim().required().min(2).max(20),
+  createdAt: Joi.date().iso().default(new Date().toISOString()), // TODO: The server will be responsible for generating the post time
+});
