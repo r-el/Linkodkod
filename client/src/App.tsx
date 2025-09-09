9b763347f90167ab9ae2b97d28f81fb8202ce8b2
Ariel <arieltanami122@gmail.com>
2025-09-09 15:24:51 +0300

    Add `/post/new` route for <PostDetailPage /> page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,15 +4,20 @@ import { Route, Routes } from "react-router-dom";
 import NotFoundPage from "./components/application-layout/NotFoundPage.tsx";
 import { Posts } from "./components/posts/Posts.tsx";
 import { PostDetailPage } from "./pages/PostDetailPage.tsx";
+import NewPostPage from "./pages/NewPostPage.tsx";
 
 export default () => {
   return (
     <AplicationLayout>
       <Routes>
         <Route index element={<Posts />} />
-        <Route path="/post/:id" element={<PostDetailPage />} />
+        <Route path="/post">
+          <Route path="new" element={<NewPostPage />} />
+          <Route path=":id" element={<PostDetailPage />} />
+        </Route>
         <Route path="/login" element={<h2>Login Page</h2>} />
         <Route path="/register" element={<h2>Register Page</h2>} />
+
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </AplicationLayout>
