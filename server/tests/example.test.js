14d7f0c78cc3eeae6cb47e2e07d519ab54057551
Ariel <arieltanami122@gmail.com>
2025-09-09 17:53:38 +0300

    Rewrite the example.test.js test file

--- server/tests/example.test.js
+++ server/tests/example.test.js
@@ -1,5 +1,5 @@
+import { test, expect } from "vitest";
+
 test("HeartBeat", async () => {
   expect(true).toBeTruthy();
 });
-
-// src: https://medium.com/@pranavpsinha/getting-started-with-tdd-test-driven-development-in-nodejs-jest-supertest-a-complete-and-60351ff5fad2
