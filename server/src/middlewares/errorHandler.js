db5e250311f21d3161986161a7394ecb2db9e89f
Ariel <arieltanami122@gmail.com>
2025-09-09 10:14:04 +0300

    Add ApiError class extends Error class with `statusCode` and `isOperational` fields

---
+++ server/src/middlewares/errorHandler.js
@@ -0,0 +1,24 @@
+/**
+ * Error Handling Middleware
+ * Centralized error handling for the application (server side)
+ *
+ * @param {number} statusCode - HTTP status code
+ * @param {string} message - Error message
+ * @param {boolean} isOperational - Indicates if the error is operational (true) or program error (false)
+ */
+
+class ApiError extends Error {
+  constructor(statusCode, message, isOperational = true) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = isOperational;
+
+    /* Captures the stack trace of the error for debugging
+        targetObject: The object that will receive the stack trace.
+        constructorOpt (Optional): The function that created the targetObject.
+    */
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export { ApiError };
