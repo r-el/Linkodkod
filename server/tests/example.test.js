30d5883f2d235393eb215e4a7798091acb012ae6
Ariel <arieltanami122@gmail.com>
2025-09-09 17:05:01 +0300

    Add an example.test.js

---
+++ server/tests/example.test.js
@@ -0,0 +1,5 @@
+test("HeartBeat", async () => {
+  expect(true).toBeTruthy();
+});
+
+// src: https://medium.com/@pranavpsinha/getting-started-with-tdd-test-driven-development-in-nodejs-jest-supertest-a-complete-and-60351ff5fad2
