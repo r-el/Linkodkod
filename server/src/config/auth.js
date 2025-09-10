fccb70cd1517486ab8753c1fa0fe4ea555e8411b
Ariel <arieltanami122@gmail.com>
2025-09-10 11:13:06 +0300

    Try convet BCRYPT_SALT_ROUNDS env variable to number, else put `10` as a default

--- server/src/config/auth.js
+++ server/src/config/auth.js
@@ -11,10 +11,17 @@ config();
 /**
  * Authentication configuration
  */
+
+// Try convet BCRYPT_SALT_ROUNDS env variable to number
+let BCRYPT_SALT_ROUNDS_INT;
+try {
+  BCRYPT_SALT_ROUNDS_INT = parseInt(process.env.BCRYPT_SALT_ROUNDS);
+} catch {}
+
 export const authConfig = {
   jwtSecret: process.env.JWT_SECRET,
   jwtExpiresIn: process.env.JWT_EXPIRES_IN || "7d",
-  bcryptRounds: process.env.BCRYPT_SALT_ROUNDS || 10,
+  bcryptSaltRounds: BCRYPT_SALT_ROUNDS_INT || 10,
 };
 
 export default authConfig;
