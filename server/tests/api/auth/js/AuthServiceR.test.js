f29351eb2c5b39a4f7cf69b22c007ffb58810eb3
Ariel <arieltanami122@gmail.com>
2025-09-10 12:22:48 +0300

    Remove unused code in AuthServiceR.test

--- server/tests/api/auth/js/AuthServiceR.test.js
+++ server/tests/api/auth/js/AuthServiceR.test.js
@@ -2,12 +2,6 @@ import { describe, expect, it, vi } from "vitest";
 import * as authService from "../../../../src/services/authService.js";
 import bcrypt from "bcrypt";
 
-// const demoPassword = "testpassword123";
-
-// authService.hashPassword(demoPassword).then((hash) => {
-//   authService.comparePassword(demoPassword, hash).then((res) => console.log(res));
-//   console.log(hash);
-// });
 describe("Hash Password", () => {
   it("should hash password successfully", async () => {
     // Arrange
