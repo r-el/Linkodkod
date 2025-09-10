776ad334581289d5b22a7275b953974d3c4f229f
Ariel <arieltanami122@gmail.com>
2025-09-10 15:17:06 +0300

    Wrap App.tsx in <AuthProvider/>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,31 @@
 import "./App.css";
 import AplicationLayout from "./components/application-layout/AplicationLayout.tsx";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NotFoundPage from "./components/application-layout/NotFoundPage.tsx";
 import { Posts } from "./components/posts/Posts.tsx";
 import { PostDetailPage } from "./pages/PostDetailPage.tsx";
 import NewPostPage from "./pages/NewPostPage.tsx";
+import Login from "./components/auth/Login.tsx";
+import { AuthProvider } from "./context/AuthContext.tsx";
 
 export default () => {
   return (
-    <AplicationLayout>
-      <Routes>
-        <Route index element={<Posts />} />
-        <Route path="/post">
-          <Route path="new" element={<NewPostPage />} />
-          <Route path=":id" element={<PostDetailPage />} />
-        </Route>
-        <Route path="/login" element={<h2>Login Page</h2>} />
-        <Route path="/register" element={<h2>Register Page</h2>} />
+    <AuthProvider>
+      <Router>
+        <AplicationLayout>
+          <Routes>
+            <Route index element={<Posts />} />
+            <Route path="/post">
+              <Route path="new" element={<NewPostPage />} />
+              <Route path=":id" element={<PostDetailPage />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<h2>Register Page</h2>} />
 
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </AplicationLayout>
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </AplicationLayout>
+      </Router>
+    </AuthProvider>
   );
 };
