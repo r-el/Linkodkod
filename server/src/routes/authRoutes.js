c6be155061cd6684787a6c01efcfa0d7d035629b
Ariel <arieltanami122@gmail.com>
2025-09-10 17:05:41 +0300

    Add auth routes

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -3,14 +3,14 @@
  * Defines all routes related to user authentication and authorization
  */
 import express from "express";
-import * as authController from "../controllers/userController";
+import { register, login } from "../controllers/userController.js";
 
 const router = express.Router();
 
 // User registration
-router.post("/register", authController.register);
+router.post("/register", register);
 
 // User login
-router.post("/login", authController.login);
+router.post("/login", login);
 
 export default router;
