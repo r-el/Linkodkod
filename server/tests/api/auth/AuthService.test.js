572bc67cfb8c811c6b97e6dc51ecfefcb2968386
Ariel <arieltanami122@gmail.com>
2025-09-10 12:41:15 +0300

    Wrap "Hash Password" suite in describe("Authentication Service" ...) and add "should return hash as a string" test case

--- server/tests/api/auth/AuthService.test.js
+++ server/tests/api/auth/AuthService.test.js
@@ -2,25 +2,38 @@ import { describe, expect, it, vi } from "vitest";
 import * as authService from "../../../src/services/authService.js";
 import bcrypt from "bcrypt";
 
-describe("Hash Password", () => {
-  it("should throw error for empty password", async () => {
-    await expect(authService.hashPassword("")).rejects.toThrow("Password cannot be empty");
-  });
+describe("Authentication Service", () => {
+  describe("Hash Password", () => {
+    it("should throw error for empty password", async () => {
+      await expect(authService.hashPassword("")).rejects.toThrow("Password cannot be empty");
+    });
 
-  it("should throw error for non-string password", async () => {
-    // Act & Assert
-    await expect(authService.hashPassword(123)).rejects.toThrow("Password must be type of string");
-  });
+    it("should throw error for non-string password", async () => {
+      // Act & Assert
+      await expect(authService.hashPassword(123)).rejects.toThrow("Password must be type of string");
+    });
 
-  it("should hash password successfully", async () => {
-    // Arrange
-    const plainPassword = "testpassword123";
+    it("should return hash as a string", async () => {
+      // Arrange
+      const plainPassword = "testpassword123";
 
-    // Act
-    const hashedPassword = await authService.hashPassword(plainPassword);
+      // Act
+      const hashedPassword = await authService.hashPassword(plainPassword);
 
-    // Assert
-    const result = await bcrypt.compare(plainPassword, hashedPassword);
-    expect(result).toBe(true);
+      // Assert
+      expect(hashedPassword).toBeTypeOf("string");
+    });
+
+    it("should hash password successfully", async () => {
+      // Arrange
+      const plainPassword = "testpassword123";
+
+      // Act
+      const hashedPassword = await authService.hashPassword(plainPassword);
+
+      // Assert
+      const result = await bcrypt.compare(plainPassword, hashedPassword);
+      expect(result).toBe(true);
+    });
   });
 });
